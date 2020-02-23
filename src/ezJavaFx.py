39a91322c981e6ee87bc95c622a4b5f5a1e85fec
zdivdev <48123528+zdivdev@users.noreply.github.com>
2020-02-23 16:41:03 +0900

    Update ezJavaFx.py

--- src/ezJavaFx.py
+++ src/ezJavaFx.py
@@ -8,23 +8,86 @@ from javafx.geometry import Insets
 from javafx.geometry import Orientation
 from javafx.geometry import Pos
 
+#
+# Control Table
+#
+
 __ctrl_table = {}
+
+#
+# Dialog
+#
+
+def Alert(title, message, stage=None):
+    from javafx.scene.control import Alert
+    from javafx.scene.control.Alert import AlertType
+    from javafx.stage import Modality
+    alert = Alert(AlertType.INFORMATION)
+    alert.setTitle(title)
+    alert.setHeaderText("")
+    alert.setContentText(message)
+    if stage: alert.initOwner(stage)
+    alert.initModality(Modality.APPLICATION_MODAL)
+    alert.showAndWait()
+
+def YesNo(title, message, stage=None):
+    from javafx.scene.control import Alert
+    from javafx.scene.control.Alert import AlertType
+    from javafx.stage import Modality
+    from javafx.scene.control import ButtonType
+    alert = Alert(AlertType.CONFIRMATION)
+    alert.setTitle(title)
+    alert.setHeaderText("")
+    alert.setContentText(message)
+    alert.initModality(Modality.APPLICATION_MODAL)
+    if stage: alert.initOwner(stage)
+    result = alert.showAndWait()
+    if result.get() == ButtonType.OK:
+        return True
+    else:
+        return False
+
+def FileDialog(initialFile, save, stage=None):
+    from javafx.stage import FileChooser
+    from java.io import File
+    dlg = FileChooser()
+    if initialFile:
+        f = File(initialFile)
+        if f.exists():
+            if f.isDirectory(): dlg.setInitialDirectory(f)
+            if f.isFile():      dlg.setInitialFileName(f.getAbsolutePath());
+    dlg.setTitle("Select File");
+    if save: return dlg.showSaveDialog(stage);
+    else:    return dlg.showOpenDialog(stage);
+
+def FileOpenDialog(initialFile, stage=None):
+    return FileDialog(initialFile, False, stage)
+
+def FileSaveDialog(initialFile, stage=None):
+    return FileDialog(initialFile, True, stage)
+
+def DirectoryOpenDialog(initialDirectory, stage=None):
+    from javafx.stage import DirectoryChooser
+    from java.io import File
+    dlg = DirectoryChooser()
+    if initialDirectory:
+        f = File(initialDirectory)
+        if f.exists() and f.isDirectory():
+            dlg.setInitialDirectory(f);
+    dlg.setTitle("Select Folder");
+    return dlg.showDialog(stage)
 
 #
 # Container
 #
 
 class FxBox():
-    def getItem(self,index):
-        return self.ctrl.getChildren().get(index)
-    def addItem(self,item,expand=False): #Node
+    def getItem(self,index): return self.ctrl.getChildren().get(index)
+    def addItem(self,item,expand=False): 
         self.ctrl.getChildren().add(item)
-        if expand == True:
-            self.setExpand(item)
-    def alignLeft(self):
-        self.setAlignLeft()
-    def alignRight(self):
-        self.setAlignRight()
+        if expand == True: self.setExpand(item)
+    def alignLeft(self): self.setAlignLeft()
+    def alignRight(self): self.setAlignRight()
     def addSeparator(self):
         from javafx.scene.control import Separator
         sep = Separator()
@@ -53,6 +116,32 @@ class FxHBox(FxBox):
         self.ctrl.setSpacing( gap )
         self.ctrl.setPadding( Insets( pad, pad, pad, pad ) )
 
+class FxSplitPane(object):
+    def addItem(self,item): self.ctrl.getItems().add(item);
+    def addItems(self,layout):
+        items = layout.get('items')
+        if items:
+            for item in items:
+                self.addItem(Layout(item).ctrl)
+        
+class FxVSplitPane(FxSplitPane):
+    def __init__(self,h):
+        from javafx.scene.control import SplitPane
+        self.ctrl = SplitPane()
+        if h.get('first'):
+            self.ctrl.setDividerPositions(h['first'], 1-h['first']);
+        self.ctrl.setOrientation(Orientation.VERTICAL);
+        self.addItems(h)
+
+class FxHSplitPane(FxSplitPane):
+    def __init__(self,h):
+        from javafx.scene.control import SplitPane
+        self.ctrl = SplitPane()
+        if h.get('first'):
+            self.ctrl.setDividerPositions(h['first'], 1-h['first']);
+        self.ctrl.setOrientation(Orientation.HORIZONTAL);
+        self.addItems(h)
+        
 #
 # Control
 #
@@ -73,27 +162,23 @@ class FxButton():
             self.ctrl.setOnAction( h['handler'] )
 
 class FxText():
-    def getText(self):
-        return self.ctrl.getText()
-    def setText(self,text):
-        self.ctrl.setText(text)
-    def insert(self,index,text):
-        self.ctrl.insertText(index,text)
-    def append(self,text):
-        self.ctrl.appendText(text)
-    def clear(self):
-        self.ctrl.clear()
-    def copy(self):
-        self.ctrl.copy()
-    def paste(self):
-        self.ctrl.paste()
+    def getText(self): return self.ctrl.getText()
+    def setText(self,text): self.ctrl.setText(text)
+    def insert(self,index,text): self.ctrl.insertText(index,text)
+    def append(self,text): self.ctrl.appendText(text)
+    def clear(self): self.ctrl.clear()
+    def copy(self): self.ctrl.copy()
+    def paste(self): self.ctrl.paste()
         
 class FxTextField(FxText):
     def __init__(self,h):
         from javafx.scene.control import TextField
         self.ctrl = TextField()
         self.ctrl.setText(h.get('text'))
-
+        SetFileDropHandler( self.ctrl, self.DropHandler )
+    def DropHandler(self,files):
+        self.ctrl.setText( files.get(0).getPath() )
+        
 class FxTextArea(FxText):
     def __init__(self,h):
         from javafx.scene.control import TextArea
@@ -104,7 +189,31 @@ class FxTextArea(FxText):
 # Window
 #
 
+def DragOver(event):
+    from javafx.scene.input import TransferMode;
+    if event.getDragboard().hasFiles():
+        event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+    event.consume();
+
+def DragDropped(handler):
+    from javafx.scene.input import DragEvent
+    from javafx.scene.input import Dragboard
+    def _DragDropped(event): #closure
+        db = event.getDragboard()
+        if db.hasFiles():
+            handler( db.getFiles() ) # List<File>
+            event.setDropCompleted(True)
+        else:
+            event.setDropCompleted(False)
+        event.consume()
+    return _DragDropped
+
+def SetFileDropHandler(ctrl,handler):  
+    ctrl.setOnDragOver( DragOver )
+    ctrl.setOnDragDropped( DragDropped(handler) )
+    
 def Layout(content):
+    print('Layout')
     vbox = FxVBox(5,5)
     for v in content:
         hbox = FxHBox(5,5)
@@ -118,6 +227,8 @@ def Layout(content):
             elif name == 'Button':    f = FxButton(h)
             elif name == 'TextField': f = FxTextField(h);
             elif name == 'TextArea':  f = FxTextArea(h);
+            elif name == 'HSplit':    f = FxHSplitPane(h);
+            elif name == 'VSplit':    f = FxVSplitPane(h);
             else: continue
             
             width = -1
@@ -145,38 +256,65 @@ class Window(Application):
         self.ctrl  = __ctrl_table
         self.stage = stage
         self.stage.setTitle("FxApp Example")
-
         v = Layout(self.content)
         self.scene = Scene(v.ctrl, 640, 400)
         self.stage.setScene(self.scene)
         self.stage.show()
-
-    def SetContent(self,content):
-        self.content = content
+    def SetContent(self,content): self.content = content
+    def Alert(self, title, message): Alert(title,message,self.stage)
+    def YesNo(title, message, stage=None): return YesNo(title,message,self.stage)
+    def FileOpenDialog(self, initialFile): return FileOpenDialog(initialFile, self.stage)
+    def FileSaveDialog(self, initialFile): return FileSaveDialog(initialFile, self.stage)
         
-
 #
 # Application
 #
 
-def onButton(event):
-    ctrl = getCtrl('text')
-    if ctrl:
-        ctrl.setText("1111")
-            
+      
 class FxApp(Window):
     def __init__(self):
         self.content = [ # vbox
             [ # hbox
                 { "name" : "Label", "label" : "Address:" },
                 { "name" : "TextField", "key" : "text", "expand" : True },
-                { "name" : "Button",  "label" : "Browse", "tooltip" : "Open File", "handler" : onButton  },
+                { "name" : "Button",  "label" : "Browse", "tooltip" : "Open File", "handler" : self.onBrowse  },
+                { "name" : "Button",  "label" : "About", "handler" : self.onAbout  },
             ],
             [ # hbox
                 { "name" : "TextArea", "expand" : True },
                 { "expand" : True },
-            ],    
+            ],
+            [ # hbox
+                { "name" : "HSplit", "first" : 0.5, "expand" : True,
+                   "items" : [
+                        [ #vbox
+                            [ #hbox
+                                { "name" : "TextArea", "expand" : True },
+                                { "expand" : True },                        
+                            ],
+                        ],
+                        [ #vbox
+                            [ #hbox
+                                { "name" : "TextArea", "expand" : True },
+                                { "expand" : True },                        
+                            ],
+                        ],
+                    ]
+                },
+                { "expand" : True },
+            ],                
         ]
 
+    def onAbout(self,event):
+        v = YesNo("Global", "Dialog")
+        if v: self.Alert("Result", "Yes")
+        else: self.Alert("Result", "No")
+
+    def onBrowse(self,event):
+        f = FileOpenDialog(None)
+        ctrl = getCtrl('text')
+        if ctrl:                
+            ctrl.setText(f.getPath())
+ 
 if __name__ == '__main__':
     Application.launch(FxApp().class, [])
